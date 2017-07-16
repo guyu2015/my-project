da181c8d3eab5827f307c39894046a055a17c429
guyu2017 <13126583842@139.com>
2017-07-16 21:13:16 +0800

    my app

--- src/components/home/HomeCloth.vue
+++ src/components/home/HomeCloth.vue
@@ -26,7 +26,51 @@
       <div id="category-banner" class="category-banner">
         <img src="http://i.lifevccdn.com/upload/DesignerMessageImg/9203af2fbef145daa142dd97bb84fae3.jpg" onerror="this.style.display='none'" class="item-pic">
       </div>
-      <div class="colths"></div>
+
+
+    </div>
+    <div class="down-list">
+      <div class="subcat-title">Rico.S的精心推荐：</div>
+      <div class="shelf-item"  style="position: relative;"><a data-v-cbe28f50="" class="item-pic-bx"><img data-v-cbe28f50="" class="item-pic" src="http://i.lifevccdn.com/upload/AppItemInfoImage/ad5bea9a90a4406ba540382f4d24e2ae_d1242x0.jpg" lazy="loaded"> <!----> <div data-v-cbe28f50="" class="item-status-empty" style="display: none;">
+        热卖脱销
+      </div></a>
+        <div class="item-panel">
+          <div class="title clearFix">
+        上开式可叠加收纳箱
+      </div>
+        <div  class="item-price">
+          <div  class="price-cont">
+            <span  class="act-tag" style="display: none;"></span>
+            <span >￥</span>
+            <span >39</span>&nbsp;
+        <span class="original-price"></span>
+            <span class="promo-lable">新</span></div>
+          <div class="item-comment">评论：0</div>
+        </div>
+      </div>
+        <div >
+          </div>
+      </div>
+      <div class="shelf-item"  style="position: relative;">
+        <a data-v-cbe28f50="" class="item-pic-bx">
+        <img data-v-cbe28f50="" class="item-pic" src="http://i.lifevccdn.com/upload/AppItemInfoImage/5f4e0a73d2254e4dbca3517b59466cbd_d1242x0.jpg" lazy="loaded"> <!----> <div data-v-cbe28f50="" class="item-status-empty" style="display: none;">
+        热卖脱销
+      </div></a> <div data-v-cbe28f50="" class="item-panel"><div data-v-cbe28f50="" class="title clearFix">
+        铝合金门后挂钩
+      </div> <div data-v-cbe28f50="" class="item-price"><div data-v-cbe28f50="" class="price-cont"><span data-v-cbe28f50="" class="act-tag" style="display: none;"></span> <span data-v-cbe28f50="">￥</span> <span data-v-cbe28f50="">35</span>&nbsp;
+        <span data-v-cbe28f50="" class="original-price"></span> <span data-v-cbe28f50="" class="promo-lable">新</span></div> <div data-v-cbe28f50="" class="item-comment">评论：0</div></div></div> <div data-v-cbe28f50=""></div></div>
+      <div class="shelf-item" style="position: relative;"><a data-v-cbe28f50="" class="item-pic-bx"><img data-v-cbe28f50="" class="item-pic" src="http://i.lifevccdn.com/upload/AppItemInfoImage/22ff9444fc364a4cb66a5b31e2eb805b_d1242x0.jpg" lazy="loaded"> <!----> <div data-v-cbe28f50="" class="item-status-empty" style="display: none;">
+        热卖脱销
+      </div></a> <div data-v-cbe28f50="" class="item-panel"><div data-v-cbe28f50="" class="title clearFix">
+        洗衣防染巾(30片)
+      </div> <div data-v-cbe28f50="" class="item-price"><div data-v-cbe28f50="" class="price-cont"><span data-v-cbe28f50="" class="act-tag" style="display: none;"></span> <span data-v-cbe28f50="">￥</span> <span data-v-cbe28f50="">19</span>&nbsp;
+        <span data-v-cbe28f50="" class="original-price"></span> <span data-v-cbe28f50="" class="promo-lable">新</span></div> <div data-v-cbe28f50="" class="item-comment">评论：13</div></div></div> <div data-v-cbe28f50=""></div></div>
+      <div data-v-cbe28f50="" data-v-6c0a93da="" class="shelf-item" data-v-20f7755b="" style="position: relative;"><a data-v-cbe28f50="" class="item-pic-bx"><img data-v-cbe28f50="" class="item-pic" src="http://i.lifevccdn.com/upload/AppItemInfoImage/22ff9444fc364a4cb66a5b31e2eb805b_d1242x0.jpg" lazy="loaded"> <!----> <div data-v-cbe28f50="" class="item-status-empty" style="display: none;">
+        热卖脱销
+      </div></a> <div data-v-cbe28f50="" class="item-panel"><div data-v-cbe28f50="" class="title clearFix">
+        洗衣防染巾(30片)
+      </div> <div data-v-cbe28f50="" class="item-price"><div data-v-cbe28f50="" class="price-cont"><span data-v-cbe28f50="" class="act-tag" style="display: none;"></span> <span data-v-cbe28f50="">￥</span> <span data-v-cbe28f50="">19</span>&nbsp;
+        <span data-v-cbe28f50="" class="original-price"></span> <span data-v-cbe28f50="" class="promo-lable">新</span></div> <div data-v-cbe28f50="" class="item-comment">评论：13</div></div></div> <div data-v-cbe28f50=""></div></div>
     </div>
   </div>
 </template>
@@ -89,7 +133,7 @@
   }
   .category-banner {
     width: 100%;
-    margin: 1.5em auto 0;
+    margin: 1em auto 0;
   }
   .item-pic {
     position: relative;
@@ -100,10 +144,97 @@
     display: block;
     vertical-align: top;
   }
-  .colths{
-    width : 100%;
-    height : 3rem;
-
+  .down-list{
+    margin: 0 auto -1em;
+  }
+  .subcat-title {
+    max-width: 96%;
+    text-align: center;
+    font-size: 0.9em;
+    color: #000;
+    line-height: 2em;
+    /*margin: 0 auto -1.2em;*/
+  }
+  .shelf-item {
+    width: 96%;
+    border: 1px solid #ddd;
+    margin:0.5rem auto 0;
+  }
+  .shelf-item .item-pic-bx {
+    width: 100%;
+    overflow: hidden;
+    display: block;
+    position: relative;
+  }
+  .shelf-item .item-pic {
+    width: 100%;
+    display: block;
+    vertical-align: top;
+    margin: 0 auto;
+    position: static;
+  }
+  .item-status-empty {
+    width: 3.5rem;
+    height: 3.5rem;
+    background-color: rgba(0,0,0,.6);
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: middle;
+    font-size: 0.7em;
+    color: #fff;
+    line-height: 3.5rem;
+    font-weight: 700;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-top: -2rem;
+    margin-left: -1.8rem;
+  }
+  .shelf-item .item-panel {
+    width: 100%;
+    background: #fff;
+    color: #333;
+    padding-bottom: .15rem;
+  }
+  .shelf-item .item-panel, .shelf-item .profile {
+    overflow: hidden;
+    font-size: 0.6rem;
+    line-height: 1rem;
+  }
+  .shelf-item .item-panel .act-tag, .shelf-item .item-panel .act-tag-red, .shelf-item .item-panel .promo-lable {
+    display: inline-block;
+    background-color: #c00;
+    padding: 1px 3px;
+  }
+  .shelf-item .item-panel .act-tag-red, .shelf-item .item-panel .promo-lable {
+    background-color: #c00;
+    color: #fff;
+  }
+  .shelf-item .item-panel .item-price, .shelf-item .item-panel .title {
+    width: 94%;
+    overflow: hidden;
+    margin: 0 auto ;
+  }
+  .shelf-item .item-panel .title {
+    text-align: left;
+    font-size: 0.6rem;
+    color: #333;
+    line-height: 1.2rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    padding-top: .25rem;
+  }
+  .shelf-item .item-panel .item-comment {
+    float: right;
+    text-align: right;
+    color: #999;
+  }
+  .shelf-item .item-panel .price-cont {
+    min-width: 68%;
+    overflow: hidden;
+    float: left;
+    line-height: 0.9rem;
+    margin-top: .1rem;
   }
 </style>
 
