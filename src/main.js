f6412802e29336bf4a78cbffbf80acab93a4cf23
guyu2017 <13126583842@139.com>
2017-07-16 21:13:54 +0800

    my app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueAwesomeSwiper  from 'vue-awesome-swiper'
+import {confirm }  from 'vue-messagebox'
 
 Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
